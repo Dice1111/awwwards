3af403d282d201f2b1ec0f638b387a980a235909
Dice <98948753+Dice1111@users.noreply.github.com>
2024-12-24 20:38:44 +0800

    navbar_section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import About from "@/components/sections/About";
 import Hero from "@/components/sections/Hero";
+import Navbar from "@/components/sections/Navbar";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar></Navbar>
       <Hero></Hero>
-
+      <About></About>
       <section className="w-full overflow-hidden h-dvh bg-blue-500" />
     </main>
   );
