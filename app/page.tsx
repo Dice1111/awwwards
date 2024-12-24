8309b18fd637488a2eb4703eca8552b3b81a9717
Dice <98948753+Dice1111@users.noreply.github.com>
2024-12-24 21:31:21 +0800

    story_section

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import About from "@/components/sections/About";
 import Feature from "@/components/sections/Feature";
 import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/sections/Navbar";
+import Story from "@/components/sections/Story";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <About></About>
-      <Feature></Feature>
+      <Navbar />
+      <Hero />
+      <About />
+      <Feature />
+      <Story />
       <section className="w-full overflow-hidden h-dvh bg-blue-500" />
     </main>
   );
